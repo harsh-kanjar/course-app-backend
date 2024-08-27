d2af56f4643d2c584de987daacd14b48ef8ee5f6
harsh-kanjar <harshlankar07@gmail.com>
2024-08-27 18:52:29 +0530

    test_api

--- courses/urls.py
+++ courses/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from .views import CourseListCreate, CourseDetail, InstanceListCreate, InstanceDetail, InstanceDetailByCourse
-from courses import views
+from .views import CourseListCreate, InstanceListCreate
 
 urlpatterns = [
-    path('courses/', CourseListCreate.as_view(), name='course-list'),
-    path('courses/<int:pk>/', CourseDetail.as_view(), name='course-detail'),
-    path('instances/', InstanceListCreate.as_view(), name='instance-list'),
-    path('instances/<int:year>/<int:semester>/', InstanceDetail.as_view(), name='instance-detail'),
-    path('instances/<int:year>/<int:semester>/<int:course_id>/', InstanceDetailByCourse.as_view(), name='instance-detail-by-course'),
-    path('api/instances/', views.InstanceListCreateView.as_view(), name='instance-list-create'),
-    path('api/instances/<int:pk>/', views.InstanceDetailView.as_view(), name='instance-detail'),
+    # URL for listing and adding courses
+    path('courses/', CourseListCreate.as_view(), name='course-list-create'),
+
+    # URL for listing and adding instances
+    path('instances/', InstanceListCreate.as_view(), name='instance-list-create'),
 ]
